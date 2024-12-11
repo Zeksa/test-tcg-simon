a89c2875b0e5f5d3be8fe02ee120afcf7c6d2a46
Simon PAGE <page.simon14@gmail.com>
2024-12-11 22:40:57 +0100

    limit addCards concurrency

--- src/functions/addCards.ts
+++ src/functions/addCards.ts
@@ -3,7 +3,7 @@ import { cardsCollection } from '../collections'
 import { PROVIDERS } from '../common/globalData'
 import { cardsNormalizers } from '../utils/cardsNormalizers'
 
-export const addCards = onRequest({ minInstances: 0, maxInstances: 1 }, async (request, response) => {
+export const addCards = onRequest({ minInstances: 0, maxInstances: 1, concurrency: 10 }, async (request, response) => {
   // In production, we would check for permissions first
 
   if (!request.body || typeof request.body !== 'object') {
